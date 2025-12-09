74dbfe6cf53b194d34a81ffe9570bc0e398b14a6
Nina Coiffin <nina.coiffin@etud.u-picardie.fr>
2025-12-09 11:30:58 +0100

    Changement CrateClassique

--- Projet/src/Jest/CarteClassique.java
+++ Projet/src/Jest/CarteClassique.java
@@ -1,5 +1,32 @@
 package Jest;
 
-public class CarteClassique {
+import Jest.Carte.Carte;
+import Jest.Carte.Symbole;
 
-}
+public class CarteClassique extends Carte {
+		
+		@Override
+		public String toString() {
+			return numero + " de " + symbole ;
+		}
+
+		public int numero;
+		public Symbole symbole;
+		
+		public CarteClassique(int n, Symbole s) {
+			super();
+			this.numero=n;
+			this.symbole=s;
+			String nom = ""+n+" "+s;
+			this.setNom(nom);		
+		}
+		
+		public int getNumero() {
+			return this.numero;
+		}
+		
+		public Symbole getSymbole() {
+			return this.symbole;
+		}
+
+	}
