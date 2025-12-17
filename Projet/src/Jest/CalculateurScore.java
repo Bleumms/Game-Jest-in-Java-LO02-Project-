a2bd79b41c3dc72963e9695baf0d5c8dc411bdbc
Bleumms <emeline.nerot@hotmail.com>
2025-12-17 10:40:31 +0100

    Implémentation visitor

--- Projet/src/Jest/CalculateurScore.java
+++ Projet/src/Jest/CalculateurScore.java
@@ -8,7 +8,13 @@ public class CalculateurScore implements Visitor{
 	
 	@Override
 	public void visit(Joueur player) {
-		
+		// Calcul du score en fonction des cartes dans la collection du joueur
+		int totalScore = 0;
+		for (Carte c : player.getCollection()) {
+			totalScore += 0; //Dois être remplacé par la méthode de calcul de score de chaque carte
+		}
+		player.setScore(totalScore);
+
 	}
 
 }
