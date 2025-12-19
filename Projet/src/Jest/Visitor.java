928990026f58dfd58af65cefedd16489464c399f
Bleumms <emeline.nerot@hotmail.com>
2025-12-19 17:11:49 +0100

    Documentation Interfaces

--- Projet/src/Jest/Visitor.java
+++ Projet/src/Jest/Visitor.java
@@ -1,8 +1,23 @@
+/*
+ * Interface du pattern Visitor pour le calcul des scores.
+ * 
+ * @author Nina et Emeline
+ * @see Visitable
+*/
+
 package Jest;
 
 public interface Visitor {
 
+	/*
+	 * Visite un joueur pour calculer son score
+	 * @param player Le joueur à visiter
+	*/
 	void visit (Joueur player);
 
+	/*
+	 * Définit la référence des règles à utiliser pour le calcul des scores
+	 * @param r La référence des règles
+	*/
 	void setReference(Reference r);
 }
