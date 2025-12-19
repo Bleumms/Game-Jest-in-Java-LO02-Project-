901b3dbeb4257c2f4f77c25d7a72197371503968
Bleumms <emeline.nerot@hotmail.com>
2025-12-19 12:15:40 +0100

    Documentation JoueurVirtuel.java
    Added documentation comments for the JoueurVirtuel class and its methods.

--- Projet/src/Jest/JoueurVirtuel.java
+++ Projet/src/Jest/JoueurVirtuel.java
@@ -1,27 +1,57 @@
+/*
+ * Représente un joueur contrôlé par l'ordinateur.
+ * Utilise une stratégie pour prendre ses décisions.
+ * 
+ * @author Nina et Emeline
+ * @version 1.0
+ * @see Joueur
+ * @see Strategie
+*/
+
 package Jest;
 
 import java.util.List;
 
 public class JoueurVirtuel extends Joueur {
 
+	/*
+	 * Stratégie utilisée par le joueur virtuel pour prendre des décisions
+	*/
 	private Strategie strat;
 
+	/* 
+	 * Constructeur avec paramètre.
+	 * Initialise le nom du joueur virtuel et sa stratégie.
+	 * @param n Le nom du joueur
+	 * @param strategie La stratégie à utiliser
+	*/
 	public JoueurVirtuel(String n, Strategie strategie) {
 		super(n);
 		this.strat = strategie;
 	}
 
+	/* 
+	 * Définit la stratégie du joueur virtuel.
+	 * @param stra La stratégie à définir
+	*/
 	public void setStrategie(Strategie stra) {
 		this.strat = stra;
 	}
 
 	// a changer avec strategie
+	/* 
+	 * Fait une offre en utilisant la stratégie définie.
+	*/
 	public void faireUneOffre() {
 		int numCarte = this.strat.executeFaireUneOffre(this.getCartesDistribuees(), this.getCollection());
 		this.choisirCarteVisible(numCarte);
-
 	}
 
+	/* 
+	 * Choisit une carte parmi les cartes des autres joueurs en utilisant la stratégie définie.
+	 * @param j La liste des autres joueurs
+	 * @return La liste des indices des cartes choisies
+	*/
 	public List<Integer> choisirUneCarte (List<Joueur> j){
 		List<Integer> res = this.strat.executeChoisirUneCarte(j, this);
 		return res;
