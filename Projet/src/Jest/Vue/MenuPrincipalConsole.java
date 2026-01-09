3b1fc864bfcf2abba35828375232dfab65ad2512
Nina Coiffin <nina.coiffin@etud.u-picardie.fr>
2026-01-09 16:44:10 +0100

    console

---
+++ Projet/src/Jest/Vue/MenuPrincipalConsole.java
@@ -0,0 +1,56 @@
+package Jest.Vue;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import Jest.Controler.MenuDebutControler;
+import Jest.Model.Menu;
+import java.util.Scanner;
+
+public class MenuPrincipalConsole implements Observer {
+
+    private Menu menu;
+    private Scanner scanner;
+
+
+    public MenuPrincipalConsole() {
+
+        this.menu = new Menu();
+		this.menu.addObserver(this);
+        scanner = new Scanner(System.in);
+
+		affichage();
+	}
+
+    private String ecouterReponse(){
+        System.out.print("Votre réponse : ");
+        return scanner.nextLine();
+    }
+
+    private void affichage(){
+        System.out.println("     JEST     -     by Nina et Emeline     \n\n");
+        System.out.println("\nQue souhaitez vous faire ?");
+		System.out.println("      1 -   Créer une nouvelle partie ! ");
+		System.out.println("      2 -   Reprendre une ancienne partie ! ");
+        
+        boolean valide = false;
+        while (!valide){
+            String rep = ecouterReponse();
+            if (rep.charAt(0)=='1'){
+                System.out.println("\nVous avez choisi de créer une nouvelle partie !");
+                valide=true;
+            } 
+            if (rep.charAt(0)=='2' ){
+                System.out.println("\nVous avez choisi de reprendre une partie !");
+                valide=true;
+            }
+        }
+    }
+
+    public void update(Observable instanceObservable, Object arg1){
+    }
+
+    public static void main(String[] args) {
+		MenuPrincipalConsole m = new MenuPrincipalConsole();
+	}
+}
