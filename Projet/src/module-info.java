fb4972ac9e80e2ba449f5115d557fe294bb3b38a
Bleumms <emeline.nerot@hotmail.com>
2026-01-16 20:58:21 +0100

    Update module-info.java, erreur nom fichier

--- Projet/src/module-info.java
+++ Projet/src/module-info.java
@@ -1,6 +1,6 @@
 /**
  * Cette application permet de jouer à un jeu du nom de Jest, un jeu open source. Une partie peut soit être lancé depuis le fichier MenuPrincipal (dans Vue) pour jouer sur l'interface graphique,
- * ou alors via le terminal de ligne de commande en exécutant le fichier MenuPrincipalCommande.
+ * ou alors via le terminal de ligne de commande en exécutant le fichier MenuPrincipalConsole.
  * Ce projet est réalisé dans le cadre du module de cours LO02 de l'UTT, qui a pour but le travail de java et de la POO.
  * Amusez vous bien!
  */
@@ -8,3 +8,4 @@ module Projet {
     requires java.desktop;
 
 }
+
