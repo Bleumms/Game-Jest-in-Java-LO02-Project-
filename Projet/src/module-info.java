0216584e4cc7150cb568280d90604d7cd26c3ebe
Bleumms <emeline.nerot@hotmail.com>
2026-01-16 20:56:31 +0100

    Update module-info.java with project description
    Added a description of the Jest game and its usage.

--- Projet/src/module-info.java
+++ Projet/src/module-info.java
@@ -1,9 +1,10 @@
 /**
- * 
- */
-/**
- * 
+ * Cette application permet de jouer à un jeu du nom de Jest, un jeu open source. Une partie peut soit être lancé depuis le fichier MenuPrincipal (dans Vue) pour jouer sur l'interface graphique,
+ * ou alors via le terminal de ligne de commande en exécutant le fichier MenuPrincipalCommande.
+ * Ce projet est réalisé dans le cadre du module de cours LO02 de l'UTT, qui a pour but le travail de java et de la POO.
+ * Amusez vous bien!
  */
 module Projet {
     requires java.desktop;
+
 }
